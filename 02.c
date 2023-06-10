be330bd3cbc358f7079c69794b34a39fb4084359
xdexst <rnoy10303@gmail.com>
2023-06-10 11:57:17 +0400

    1.2

---
+++ 02.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+int main () {
+int a = 8;
+int b = 6;
+int S ;
+int p;
+int c;
+int d;
+d=a*a+b*b;
+c= sqrt(d);
+S= (a*b)/2 ;
+p= a + b + c;
+printf("Площадь треугольника:%d, Периметр треугоьника:%d.\n",S,p);
+return 0;
+}
